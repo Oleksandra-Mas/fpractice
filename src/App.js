fce4d2e528cb9257dd24db0500da8c5bb41cdef9
Korotenko Alex <alexey.korotenko90@gmail.com>
2022-01-29 10:07:10 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import PropTypes from "prop-types";
 
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import { Container } from "./components/Container";
 import { Features } from "./components/features/features";
 
 function App() {
   return (
-    <div>
+    <Container>
       <Header />
       <Hero />
       <Features />
-    </div>
+    </Container>
   );
 }
 
